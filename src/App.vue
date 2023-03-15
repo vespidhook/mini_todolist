85607e30a31ad4e10d31927390c0fc5db944d9f6
Bruno Alves <vespidhook@gmail.com>
2023-03-15 13:51:07 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<!-- Content -->
+    <div class="px-3 py-10 md:px-10">
+        <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
+            <TodoSpinner />
+
+            <TodoFormAdd />
+
+            <TodoItems />
+
+            <TodoEmpty />
+        </div>
+    </div>
+    <!--/ Content -->
+</template>
+
+<script>
+import TodoFormAdd from './components/TodoFormAdd.vue';
+import TodoSpinner from './components/TodoSpinner.vue';
+import TodoItems from "@/components/TodoItems";
+import TodoEmpty from "@/components/TodoEmpty";
+
+export default {
+    name: "App",
+    components: {TodoEmpty, TodoItems, TodoSpinner, TodoFormAdd }
+}
+</script>
---
+++ src/components/TodoItems.vue
@@ -0,0 +1,19 @@
+
+import TodoItem from './TodoItem.vue';
+
+<template>
+  <div class="space-y-2">
+    <TodoItem />
+    <TodoItem />
+    <TodoItem />
+    <TodoItem />
+  </div>
+</template>
+
+<script>
+  import TodoItem from '@/components/TodoItem';
+  export default {
+    components: { TodoItem }
+  }
+</script>
+
