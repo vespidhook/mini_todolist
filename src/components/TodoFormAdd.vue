7a35643352def3dcb610597b018e5e409d1320b5
Bruno Alves <vespidhook@gmail.com>
2023-03-15 20:00:19 -0300

    ...

--- src/components/TodoFormAdd.vue
+++ src/components/TodoFormAdd.vue
@@ -2,14 +2,11 @@
   <form
       @submit.stop.prevent="addTodo"
       class="flex items-center px-4 bg-gray-900 h-15 rounded-sm border-l-2 border-green-400 mb-3">
-    {{ title}}
                 <input
                     v-model="title"
                     placeholder="Adicione um novo item ..."
                     type="text"
-                    class="bg-gray-900 placeholder-gray-500 text-gray-500 
-font-light focus:outline-none block w-full appearance-none leading-normal 
-py-3 pr-3"
+                    class="bg-gray-900 placeholder-gray-500 text-gray-500 font-light focus:outline-none block w-full appearance-none leading-normal py-3 pr-3"
                 >
 
                 <button
@@ -32,7 +29,15 @@ focus:outline-none"
 
     methods: {
       addTodo() {
-        console.log('here', this.title);
+        if (!this.title) {
+          return;
+        }
+        this.$store.dispatch('addTodo', {
+          title: this.title,
+          completed: false
+        }).finally(() => {
+          this.title = '';
+        });
       }
     }
   }
--- src/store/index.js
+++ src/store/index.js
@@ -10,6 +10,22 @@ export default createStore({
   mutations: {
     storeTodos(state, payload) {
       state.todos = payload
+    },
+
+    storeTodo(state, payload) {
+      const index = state.todos.findIndex(todo => todo.id === payload.id)
+      if (index >= 0) {
+        state.todos.splice(index, 1, payload)
+      } else {
+        state.todos.push(payload)
+      }
+    },
+
+    deleteTodo(state, id) {
+      const index = state.todos.findIndex(todo => todo.id === id)
+      if (index >= 0) {
+        state.todos.splice(index, 1)
+      }
     }
   },
   actions: {
@@ -25,9 +41,23 @@ export default createStore({
       })
     },
 
-    addTodo() {
-      axios.post('http://localhost:3000/todos', data)
-    }
+    addTodo({ commit }, data) {
+      return axios.post('http://localhost:3000/todos', data).then((response) => {
+        commit('storeTodo', response.data);
+      })
+    },
+
+    updateTodo({commit}, { id, data }) {
+      return axios.put(`http://localhost:3000/todos/${id}`, data).then((response) => {
+        commit('storeTodo', response.data);
+      })
+    },
+
+    deleteTodo({commit}, id ) {
+      return axios.delete(`http://localhost:3000/todos/${id}`).then(() => {
+        commit('deleteTodo', id);
+      })
+    },
   },
   modules: {
   }
